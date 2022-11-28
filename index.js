53c5feb721890747daa55c1dc9780b4c06530967
harsha9724 <harshahr9724@gmail.com>
2022-11-28 19:15:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const port=5000 || process.env.PORT;
+const userRouter=require("./Routes/user");
+app.use("/",userRouter);
+app.listen(port,()=>{
+   mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true},()=>{
+    console.log("connected to database");
+})
+    console.log(`server is up at ${port}`);
+})
