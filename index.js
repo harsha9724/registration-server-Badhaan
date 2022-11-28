9d292cbc5359277c72130dc5402663e3b3c38b28
harsha9724 <harshahr9724@gmail.com>
2022-11-28 21:59:56 +0530

    remove versile configuration

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ dotenv.config();
 const port=5000 || process.env.PORT;
 const userRouter=require("./Routes/user");
 app.use("/",userRouter);
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
 app.listen(port,()=>{
    mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true},()=>{
     console.log("connected to database");
